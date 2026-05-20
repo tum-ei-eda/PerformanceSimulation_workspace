04fcb968630eeefd045416e7c9fabf91704ff319
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-05-20 10:18:52 +0200

    scripts/setup_workspace.sh: use python3 instead of python3.10

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -6,17 +6,18 @@ set -e
 . $(dirname "${0}")/../.env
 
 # Check requirements
-if ! command -v python3.10 &> /dev/null; then
-    echo "Error: Python3.10 (Currently required by M2-ISA-R) is not installed on this system. Install it and try again."
+if ! command -v python3 &> /dev/null; then
+    echo "Error: Python3 (Currently required by M2-ISA-R) is not installed on this system. Install it and try again."
     exit 1
 fi
+which python3
 
 # Setup etiss-perf-sim
 ${PSW_PERF_SIM}/setup_simulator.sh
 
 # Setup M2-ISA-R
 cd ${PSW_M2ISAR}
-python3.10 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -e .
 deactivate
